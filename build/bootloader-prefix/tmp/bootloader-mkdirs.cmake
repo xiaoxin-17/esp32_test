199d92182b93c30649ded171d81420d0885b2e2e
xiaoxin-17 <1729917474@qq.com>
2026-09-02 23:50:29 +0800

    1.提交第一个新建工程

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "E:/SoftAppE/Espressif/frameworks/esp-idf-v5.4/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "E:/SoftAppE/Espressif/frameworks/esp-idf-v5.4/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader"
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix"
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/tmp"
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/src/bootloader-stamp"
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/src"
+  "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "F:/study/Project/13_esp32/00/00_esp32/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
